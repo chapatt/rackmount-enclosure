d9abebbf49a6d7e188e776f855a9d776e870599a
Chase Patterson <chapatt@gmail.com>
2020-07-30 20:17:34 -0400

    Exclude unit setting from DXF if not set

--- src/Dxf/DxfDocument.js
+++ src/Dxf/DxfDocument.js
@@ -14,8 +14,10 @@ export default class DxfDocument extends DxfEntitiesMixin(Object) {
     let dxf = new Dxf();
 
     dxf.beginSection('HEADER');
-      dxf.addTag(9, '$MEASUREMENT');
-      dxf.addTag(70, this.units === 'English' ? 0 : 1);
+      if (this.units) {
+        dxf.addTag(9, '$MEASUREMENT');
+        dxf.addTag(70, this.units === 'English' ? 0 : 1);
+      }
       dxf.addTag(9, '$DIMASZ');
       dxf.addTag(40, 0.125);
       dxf.addTag(9, '$DIMAUNIT');
@@ -56,61 +58,5 @@ export default class DxfDocument extends DxfEntitiesMixin(Object) {
 
   addBlockReference(name, x, y, layer='0') {
     this.blockReferences.push({ name, x, y, layer });
-  }
-
-  addMinimum(dxf) {
-    dxf.beginSection('HEADER');
-      dxf.addTag(9, '$ACADVER')
-      dxf.addTag(1, 'AC1021');
-
-      dxf.addTag(9, '$HANDSEED');
-      dxf.addTag(5, '20000');
-
-      dxf.addTag(9, '$MEASUREMENT');
-      dxf.addTag(70, 0);
-    dxf.endSection(); // HEADER
-
-    dxf.beginSection('CLASSES');
-    dxf.endSection(); // CLASSES
-
-    dxf.beginSection('TABLES');
-      dxf.beginTable('VPORT');
-        dxf.addTag(2, '*ACTIVE')
-        dxf.addTag(70, 0);
-      dxf.endTable();
-      dxf.beginTable('LTYPE');
-        // BYBLOCK
-        // BYLAYER
-        // CONTINUOUS
-      dxf.endTable();
-      dxf.beginTable('LAYER');
-        // ‘0’
-      dxf.endTable();
-      dxf.beginTable('STYLE');
-        // STANDARD
-      dxf.endTable();
-      dxf.beginTable('VIEW');
-      dxf.endTable();
-      dxf.beginTable('UCS');
-      dxf.endTable();
-      dxf.beginTable('APPID');
-        // ACAD
-      dxf.endTable();
-      dxf.beginTable('DIMSTYLE');
-        // STANDARD
-      dxf.endTable();
-      dxf.beginTable('BLOCK_RECORDS');
-        // *MODEL_SPACE
-        // *PAPER_SPACE
-      dxf.endTable();
-    dxf.endSection(); // TABLES
-
-    dxf.beginSection('BLOCKS');
-      // *MODEL_SPACE
-      // *PAPER_SPACE
-    dxf.endSection(); // BLOCKS
-
-    dxf.beginSection('OBJECTS');
-    dxf.endSection(); // OBJECTS
   }
 }
