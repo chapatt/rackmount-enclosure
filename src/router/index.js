a84506463686f15c3032ed34602838c7bc9ae3da
Chase Patterson <chapatt@gmail.com>
2021-01-16 22:24:07 -0500

    Add home screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Calculator from '../views/Calculator';
+import Home from '../views/Home';
 
 Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/calculator',
     name: 'Calculator',
     component: Calculator,
   },
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="home">
+    <h1>Rackmount Enclosure</h1>
+    <router-link to="calculator">Calculator</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+      };
+    },
+    computed: {
+      aComputedProp() {
+        return null;
+      },
+    },
+    methods: {
+      aMethod() {
+      },
+    },
+    created() {
+    },
+  }
+</script>
+
+<style>
+  html {
+    font-family: sans-serif;
+  }
+  input, fieldset, label, button, select {
+    margin: 0.5em 0;
+  }
+  fieldset, .dxf-view {
+    border-width: 2px;
+    border-color: darkgray;
+    border-style: solid;
+  }
+  label.not-implemented {
+    color: #888;
+  }
+  input, button {
+    margin-right: 1em;
+  }
+  button, .dxf-view {
+    margin-bottom: 1em;
+  }
+  .dxf-view {
+    height: 30em;
+    padding: 1em;
+  }
+</style>
