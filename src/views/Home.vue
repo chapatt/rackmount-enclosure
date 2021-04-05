4a374d1c4fdd32820948e762f6c4540e224cadd3
Chase Patterson <chapatt@gmail.com>
2021-04-04 20:03:30 -0400

    Add 3D model component to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,17 @@
   <div id="home">
     <h1>Rackmount Enclosure</h1>
     <router-link to="calculator">Calculator</router-link>
+    <demo-3d />
   </div>
 </template>
 
 <script>
+  import Demo3d from '../components/Demo3d';
+
   export default {
     name: 'Home',
-    data() {
-      return {
-      };
-    },
-    computed: {
-      aComputedProp() {
-        return null;
-      },
-    },
-    methods: {
-      aMethod() {
-      },
-    },
-    created() {
+    components: {
+      Demo3d
     },
   }
 </script>
@@ -30,25 +21,10 @@
   html {
     font-family: sans-serif;
   }
-  input, fieldset, label, button, select {
-    margin: 0.5em 0;
-  }
-  fieldset, .dxf-view {
-    border-width: 2px;
-    border-color: darkgray;
-    border-style: solid;
-  }
-  label.not-implemented {
-    color: #888;
-  }
-  input, button {
-    margin-right: 1em;
-  }
-  button, .dxf-view {
-    margin-bottom: 1em;
-  }
-  .dxf-view {
+  .demo-3d {
+    width: 40em;
     height: 30em;
-    padding: 1em;
+    overflow: hidden;
+    background: linear-gradient(#382c7a, #9690b8);
   }
 </style>
